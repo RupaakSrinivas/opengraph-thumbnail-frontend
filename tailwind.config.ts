a0684f0974251716803a30f00ec179d42f5ef0d8
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2024-01-19 02:16:49 +0530

    fix: linting errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,12 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
